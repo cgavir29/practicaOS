b31e1c4b117e52514bf2ac307b0f5317cda0e990
Camilo Gaviria <camilogaviria@Camilos-MacBook-Pro.local>
2019-06-06 18:12:17 -0500

    Mapeando

--- evaluador/src/errs.cpp
+++ evaluador/src/errs.cpp
@@ -7,19 +7,19 @@ using namespace std;
 void not_enough_args() 
 {
     cout << "Not enough arguments were given" << endl;
-    exit(0);
+    exit(1);
 }
 
 void command_not_supported(const string& command) 
 {
     cout << "Command '" + command + "' not supported" << endl;
-    exit(0);
+    exit(1);
 }
 
 void option_not_supported(const string& option) 
 {
     cout << "Option '" << option << "' not supported" << endl;
-    exit(0);
+    exit(1);
 }
 
 void check_value(const string& option, const string& val) 
@@ -27,19 +27,19 @@ void check_value(const string& option, const string& val)
     if (val.empty()) 
     {
         cout << "No value given for option '" << option << "'" << endl;
-        exit(0);
+        exit(1);
     }
 }
 
 void string_not_num(const string& val) 
 {
     cout << "Invalid value, '" << val << "' must be a number" << endl;
-    exit(0);
+    exit(1);
 }
 
 void num_not_positive(int num) 
 {
     cout << "Invalid value, '" << num << "' must be postive" << endl;
-    exit(0);
+    exit(1);
 }
 
--- evaluador/src/evaluador.cpp
+++ evaluador/src/evaluador.cpp
@@ -1,5 +1,13 @@
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <semaphore.h>
 #include <iostream>
-#include <string>
+#include <cstdlib>
+#include <cstring>
 #include "errs.h"
 #include "cast.h"
 #include "init.h"
@@ -29,6 +37,15 @@ int main(int argc, char* argv[])
         if (command == "init")
         {
             handle_init(2, argc, argv, headr);
+
+            int fd = shm_open(headr.n , O_RDWR | O_CREAT | O_EXCL, 0660);
+            // if (ftruncate(fd, sizeof(struct Buffer)) != 0) {
+            //     cerr << "Error creando la memoria compartida: "
+	        //         << errno << strerror(errno) << endl;
+            //     exit(1);
+            // }
+            void *dir = mmap(NULL, sizeof(struct Header), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
         }
         else if (command == "reg")
         {
