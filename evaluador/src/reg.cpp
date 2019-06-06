4c6f7d7c3e5cbda05a125b3156578bf413bc2ca0
Camilo <cgavir29@eafit.edu.co>
2019-06-06 12:16:02 -0500

    Manejo de ficheros comando reg

--- evaluador/src/reg.cpp
+++ evaluador/src/reg.cpp
@@ -1,46 +1,13 @@
 #include <iostream>
-#include "errs.h"
-#include "cast.h"
+#include <fstream>
+#include <string>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <string>
-#include <fstream>
+#include "errs.h"
+#include "cast.h"
 #include "reg.h"
 
 using namespace std;
-
-// void handle_reg_opt_n(string shared_mem)
-// {
-
-// }
-
-// void handle_reg(int start, int end, char *argv[]) 
-// {
-//     for (int i = start; i < end; i++)
-//     {
-//         string option = argv[i];
-//         string shared_mem;
-
-//         if (i + 1 < end) 
-//         {
-//             shared_mem = argv[i + 1];
-//         }
-
-//         if (option == "-n")
-//         {
-//             check_value(option, shared_mem);
-//             handle_reg_opt_n(shared_mem);
-//             i++;
-//             continue;
-//         } 
-//         else
-//         {
-//             option_not_supported(option);
-//         }
-        
-        
-//     }
-// }
 
 // Mira si el fichero existe, sacado de https://stackoverflow.com/questions/12774207/fastest-way-to-check-if-a-file-exist-using-standard-c-c11-c
 // bool exists_test3 (const string& name) {
@@ -51,11 +18,39 @@ using namespace std;
 void handle_reg_files(int start, int end, char* argv[])
 {
     cout << "Handling Files" << endl;
+
+    ifstream infile;
+    ofstream outfile;
+
+    int bandeja_i;  
+    char tipo_muestra;
+    int cantidad;
+
     for (int i = start; i < end; i++)
     {
-        // cout << exists_test3(argv[i]) << endl;
-        cout << argv[i] << endl;
+        string infile_name = argv[i];
+        infile.open(infile_name);
+        // ifstream infile(infile_name);
+
+        size_t lastindex = infile_name.find_last_of("."); 
+        string rawname = infile_name.substr(0, lastindex);
+        outfile.open(rawname + ".spl");
+        // ofstream outfile(infile_name + ".spl");
+
+        // cout << argv[i] << endl;
+
+        while (infile >> bandeja_i >> tipo_muestra >> cantidad) 
+        {
+            
+            // cout << bandeja_i << tipo_muestra << cantidad << endl;
+
+            outfile << 3 << endl; // obviamente tiene que ser el que se genere
+        }
+
+        infile.close();
+        outfile.close();
     }
+    
 }
 
 void handle_reg(int size, char *argv[]) 
@@ -66,33 +61,25 @@ void handle_reg(int size, char *argv[])
     if (size == 4) 
     {
         // Modo Interactivo
-        string input;
         int bandeja_i;
         char tipo_muestra;
         int cantidad;
         
         cout << "> ";
-        getline(cin, input);
+        cin >> bandeja_i >> tipo_muestra >> cantidad;
 
-        while (!cin.eof())
-        {   
-            string aux = "";
-            bandeja_i = string_cast_pos(aux += input[0]);
-            aux.clear();
-            tipo_muestra = input[2];
-            cantidad = string_cast_pos(aux += input[4]);
+        while (!cin.eof()) 
+        {
 
             cout << "> ";
-            getline(cin, input);
+            cin >> bandeja_i >> tipo_muestra >> cantidad;
+
         }
         
-    } 
+    }
     else
     {
         handle_reg_files(4, size, argv);
     }
     
 }
-
-
-
