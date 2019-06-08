84463ba542a752ec86027f520835ee3a6c403f32
sarredondq <sarredondq@eafit.edu.co>
2019-06-08 11:36:00 -0500

    productor

--- evaluador/src/reg.cpp
+++ evaluador/src/reg.cpp
@@ -1,10 +1,17 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <semaphore.h>
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
 #include "errs.h"
 #include "cast.h"
+#include "structs.h"
 #include "reg.h"
 
 using namespace std;
@@ -14,6 +21,51 @@ using namespace std;
 //   struct stat buffer;   
 //   return (stat (name.c_str(), &buffer) == 0); 
 // }
+int reg_reg(int bandeja_i, char tipo_muestra,int cantidad) 
+{
+    string ban_pos = to_string(bandeja_i);
+    string be_vacios = "BEV" + ban_pos;
+    string be_llenos = "BEL" + ban_pos;
+    string be_mutex = "BEM" + ban_pos;
+
+    sem_t *vacios, *llenos, *mutex;
+
+    vacios = sem_open(be_vacios.c_str(), 0);
+    llenos = sem_open(be_llenos.c_str(), 0);
+    mutex = sem_open(be_mutex.c_str(),0);
+    int fd = shm_open("/evaluador", O_RDWR, 0660); 
+
+    if (fd < 0) {
+    cerr << "Error abriendo la memoria compartida: "
+	 << errno << strerror(errno) << endl;
+    exit(1);
+  }
+
+  void *dir;
+
+  if ((dir = mmap(NULL, sizeof(struct Evaluador), PROT_READ | PROT_WRITE, MAP_SHARED,
+		  fd, 0)) == MAP_FAILED) {
+    cerr << "Error mapeando la memoria compartida: "
+	 << errno << strerror(errno) << endl;
+    exit(1);
+  }
+  struct Evaluador *pEval = (struct Evaluador *) dir;
+
+  for(;;) {
+
+    sem_wait(vacios);
+    sem_wait(mutex);
+
+    pEval->ban_en.bandejas[bandeja_i].buffer[pEval->ban_en.bandejas[bandeja_i].entra].reactivo = tipo_muestra;
+    pEval->ban_en.bandejas[bandeja_i].buffer[pEval->ban_en.bandejas[bandeja_i].entra].cantidad = cantidad;
+    pEval->ban_en.bandejas[bandeja_i].entra = (pEval->ban_en.bandejas[bandeja_i].entra + 1) % bandeja_i;
+    pEval->ban_en.bandejas[bandeja_i].cantidad++;
+
+    sem_post(mutex);
+    sem_post(llenos);
+  }
+
+}
 
 void handle_reg_files(int start, int end, char* argv[])
 {
