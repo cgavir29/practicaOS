c4b114fa7c5fe213245168cdbb70aaf23bc91a14
sarredondq <sarredondq@eafit.edu.co>
2019-06-08 16:09:46 -0500

    igresando en colas internas

--- evaluador/src/reg.cpp
+++ evaluador/src/reg.cpp
@@ -82,6 +82,76 @@ void handle_reg_files(int start, int end, char *argv[], Evaluador *pEval)
         outfile.close();
     }
 }
+void handle_evaluate(int ban_i, Evaluador *pEval)
+{
+  string ban_pos = to_string(ban_i);
+  string be_vacios = "BEV" + ban_pos;
+  string be_llenos = "BEL" + ban_pos;
+  string be_mutex = "BEM" + ban_pos;
+
+  sem_t *vacios, *llenos, *mutex;
+
+  vacios = sem_open(be_vacios.c_str(), 0);
+  llenos = sem_open(be_llenos.c_str(), 0);
+  mutex = sem_open(be_mutex.c_str(), 0);
+
+  int id_tem;
+  char reactivo_tem;
+  int cantidad_tem;
+
+  sem_wait(llenos);
+  sem_wait(mutex);
+
+  id_tem = pEval->ban_en.bandejas[ban_i].buffer[pEval->ban_en.bandejas[ban_i].sale].id;
+  reactivo_tem = pEval->ban_en.bandejas[ban_i].buffer[pEval->ban_en.bandejas[ban_i].sale].tipo;
+  cantidad_tem = pEval->ban_en.bandejas[ban_i].buffer[pEval->ban_en.bandejas[ban_i].sale].cantidad;
+
+  pEval->ban_en.bandejas[ban_i].buffer[pEval->ban_en.bandejas[ban_i].sale].cantidad = 0;
+
+  pEval->ban_en.bandejas[ban_i].sale = (pEval->ban_en.bandejas[ban_i].sale + 1) % pEval->hdr.ie;
+  pEval->ban_en.bandejas[ban_i].cantidad--;
+
+  sem_post(mutex);
+  sem_post(vacios);
+
+    
+  int cola_int;
+  if(reactivo_tem == 'B'){
+    // Semaforos Reactivo B  
+    vacios = sem_open("BIVB",0);
+    llenos = sem_open("BILB",0);
+    mutex = sem_open("BIMB",0);
+    cola_int = 0;
+
+  }else if (reactivo_tem == 'D')
+    {
+      // Semaforos Reactivo D
+      vacios = sem_open("BIVD",0);
+      llenos = sem_open("BILD",0);
+      mutex = sem_open("BIMD",0);
+      cola_int = 1;
+    }else if(reactivo_tem == 'S')
+    {
+      // Semaforos Reactivo S
+      vacios = sem_open("BIVS",0);
+      llenos = sem_open("BILS",0);
+      mutex = sem_open("BIMS",0);
+      cola_int = 2;
+    }
+
+  sem_wait(vacios);
+  sem_wait(mutex);
+
+  pEval->ban_in.bandejas[cola_int].buffer[pEval->ban_in.bandejas[cola_int].entra].id = id_tem;
+  pEval->ban_in.bandejas[cola_int].buffer[pEval->ban_in.bandejas[cola_int].entra].tipo = reactivo_tem;
+  pEval->ban_in.bandejas[cola_int].buffer[pEval->ban_in.bandejas[cola_int].entra].cantidad = cantidad_tem;
+  pEval->ban_in.bandejas[cola_int].entra = (pEval->ban_in.bandejas[cola_int].entra + 1) % pEval->hdr.q;
+  pEval->ban_in.bandejas[cola_int].cantidad++;
+
+  sem_post(mutex);
+  sem_post(llenos);    
+
+}
 
 void handle_reg(int size, char *argv[])
 {
@@ -132,6 +202,6 @@ void handle_reg(int size, char *argv[])
     {
         handle_reg_files(4, size, argv, pEval);
     }
-
+    handle_evaluate(1,pEval);
     close(fd);
 }
